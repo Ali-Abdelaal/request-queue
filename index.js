cd27011e164d05be5afd4e813973bbf7b16c365a
ali.abd-elaal@live.com <ali.abd-elaal@live.com>
2017-01-21 20:14:52 -0900

    Edit resolve type

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var producer = require('./lib/producer');
 producer.init();
 
 setInterval(function () {
-    var request = producer.buildRequest("127.0.0.1", "ping")
+    var request = producer.buildRequest("1.0.0.45", 3)
     producer.sendRequest(request);
 }, 500);
