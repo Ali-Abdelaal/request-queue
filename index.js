c71a1cccbf44a1dd1cef1d8a585a77c00e7a1c1d
ali.abd-elaal@live.com <ali.abd-elaal@live.com>
2017-01-21 23:33:42 -0900

    Add resolve type enum

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const constants = require('./lib/constants');
 var producer = require('./lib/producer');
 
 producer.init();
 
 setInterval(function () {
-    var request = producer.buildRequest("1.0.0.45", 3)
+    var request = producer.buildRequest("1.0.0.45", constants.resolveType.ping)
     producer.sendRequest(request);
 }, 500);
